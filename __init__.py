3eec1ff5c37f94e8ea3386da6771a0c0fb103e88
Dennis Schöneberg <schoenebergde@gmail.com>
2023-12-06 10:29:08 +0100

    commit

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
 """
 @author: Dennis
 @title: ComfyUI_Guide_To_Making_Custom_Nodes
-@description: Akatsuzi's Tutorial
+@description: Akatsuzi's TutorialAkatsuzi's Tutorial
 """
 
 from .nodes.nodes import *
 
 NODE_CLASS_MAPPINGS = {
     "Print Hello World": PrintHelloWorld,
+    "Concatenate Hello World": ConcatenateHelloWorld,
+    "Hello World Overlay Text": HelloWorldOverlayText,
     }
     
-print("\033[34mComfyUI Tutorial Nodes: \033[92mLoaded\033[0m")
+print("\033[34mComfyUI Tutorial Nodes: \033[92mLoaded\033[0m")    
--- nodes/nodes.py
+++ nodes/nodes.py
@@ -1,9 +1,18 @@
-class PrintHelloWorld:
+import numpy as np
+import torch
+from PIL import Image, ImageDraw, ImageFont
+
+def pil2tensor(image):
+    return torch.from_numpy(np.array(image).astype(np.float32) / 255.0).unsqueeze(0) 
+ 
+# based on https://stackoverflow.com/questions/1970807/center-middle-align-text-with-pil
+
+class PrintHelloWorld:     
 
     @classmethod
     def INPUT_TYPES(cls):
-
-               return {"required": {
+               
+        return {"required": {       
                     "text": ("STRING", {"multiline": False, "default": "Hello World"}),
                     }
                 }
@@ -16,31 +25,73 @@ class PrintHelloWorld:
     def print_text(self, text):
 
         print(f"Tutorial Text : {text}")
-       
+        
         return {}
-    
-    class ConcatenateHelloWorld:
 
+    # Method to print the text
     @classmethod
-    def INPUT_TYPES(cls):
-
-    RETURN_TYPES = ("STRING",)
-    FUNCTION = "concatenate_text"
-    CATEGORY = "Tutorial Nodes"
-
-    def concatenate_text(self, text1, text2):
-       
-        return {}
+    def print_text(cls, text):
+        print(f"Tutorial Text: {text}")
 
 class ConcatenateHelloWorld:
-
+    # Definition of input parameters
     @classmethod
     def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "text1": ("STRING", {"multiline": False, "default": "Hello"}),
+                "text2": ("STRING", {"multiline": False, "default": "World"}),
+            }
+        }
 
     RETURN_TYPES = ("STRING",)
     FUNCTION = "concatenate_text"
     CATEGORY = "Tutorial Nodes"
 
-    def concatenate_text(self, text1, text2):
-       
-        return {}
+    # Method to concatenate text
+    @classmethod
+    def concatenate_text(cls, text1, text2):
+        text_out = text1 + " " + text2
+        return (text_out,)
+
+class HelloWorldOverlayText:
+
+    @classmethod
+    def INPUT_TYPES(cls):
+               
+        return {"required": {
+                    "image_width": ("INT", {"default": 512, "min": 64, "max": 2048}),
+                    "image_height": ("INT", {"default": 512, "min": 64, "max": 2048}),        
+                    "text": ("STRING", {"multiline": True, "default": "Hello World"}),
+                    "font_size": ("INT", {"default": 50, "min": 1, "max": 1024}),
+                    "font_color": (["white", "black", "red", "green", "blue", "yellow"],),
+                    "background_color": (["white", "black", "red", "green", "blue", "yellow"],),
+                    }
+                }
+
+    RETURN_TYPES = ("IMAGE",)
+    #RETURN_NAMES = ("IMAGE",)
+    FUNCTION = "draw_overlay_text"
+    CATEGORY = "Tutorial Nodes"
+
+    def draw_overlay_text(self, image_width, image_height, text, 
+                   font_size, font_color, background_color):
+
+        # Create a new PIL image
+        new_img = Image.new("RGBA", (image_width, image_height), background_color) 
+        draw = ImageDraw.Draw(new_img)
+
+        # Define font
+        font = ImageFont.truetype("arial.ttf", size=font_size) 
+        
+        # Get the image center
+        image_center_x = image_width/2
+        image_center_y = image_height/2
+        
+        # Draw the text, mm = text center
+        draw.text((image_center_x, image_center_y), text, fill=font_color, font=font, anchor="mm")
+        
+        # Convert the PIL image to a torch tensor
+        image_out = pil2tensor(new_img)
+        
+        return (image_out,)
